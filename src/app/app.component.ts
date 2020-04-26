d8e2e84fff9b20fffbd309dded514d0d475468f3
COG <apple@COG-MBP.local>
2020-04-26 19:53:41 +0530

    Converting the website to Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vishnunairportfolio';
+  faCoffee = faCoffee;  
+  
+}
