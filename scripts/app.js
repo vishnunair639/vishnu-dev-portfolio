bddc54d4e5f360bcbeddf361924472516a25cbe7
COG <apple@COG-MBP.local>
2021-02-13 01:19:38 +0530

    Adding animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
 window.animatelo.slideInLeft('#hero-info');
 window.animatelo.slideInDown('#main-navbar');
 window.animatelo.lightSpeedIn('#hero-working');
+window.animatelo.slideInUp('#footer-main'); 
