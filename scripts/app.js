66c4c8d5fd9e785f66b52f82e3c6328d2242ee39
COG <apple@COG-MBP.local>
2021-02-13 21:13:57 +0530

    UI modifications

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
 window.animatelo.slideInLeft('#hero-info');
 window.animatelo.slideInDown('#main-navbar');
-window.animatelo.lightSpeedIn('#hero-working');
+window.animatelo.flip('#hero-working');
 window.animatelo.slideInUp('#footer-main'); 
